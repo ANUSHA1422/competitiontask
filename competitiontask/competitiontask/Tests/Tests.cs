208e4899d97ed27e93bbc8e88426de833aae0810
anusha <anusweetie.me@gmail.com>
2022-11-17 23:25:32 +0800

    pages,tests

---
+++ competitiontask/competitiontask/Tests/Tests.cs
@@ -0,0 +1,46 @@
+﻿using Competition.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Competition.Tests
+{
+    internal class Tests : Global.Base
+    {
+        ManageListings manageListingObj;
+        public Tests()
+        {
+            manageListingObj = new ManageListings();
+        }
+        [Test]
+        public void EnterShareSkill()
+        {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            manageListingObj.AddListing();
+            verifyAddListing();
+        }
+
+        [Test]
+        public void EditListing()
+        {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+
+
+
+
+
+        public void verifyAddListing()
+        {
+
+        }
+
+    }
+}
+
+
+
+
